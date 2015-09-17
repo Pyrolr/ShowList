2aff56fd3c496fa68addc7551c2978a4a1812cc7
Hardik <adolfhardik@gmail.com>
2015-09-17 21:50:07 +0530

    Intial

---
+++ MoviewShowcase/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MoviewShowcase
+//
+//  Created by Hardik on 15/09/15.
+//  Copyright © 2015 HDG. All rights reserved.
+//
+
+import Foundation
---
+++ MoviewShowcase/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  MoviewShowcase
+//
+//  Created by Hardik on 15/09/15.
+//  Copyright © 2015 HDG. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
